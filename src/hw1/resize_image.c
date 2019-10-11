4ec42e41a16b855d556931c28a3079f2f5635043
Chanwut Kittivorawong <chanwutk@gmail.com>
2019-10-11 15:39:11 -0700

    cleanup resize

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -29,14 +29,22 @@ float bilinear_interpolate(image im, float x, float y, int c) {
   return 0;
 }
 
+void find_bound(int *bound, float current_pos, int size) {
+  bound[0] = (int)current_pos;
+  bound[1] = cap_index(bound[0] + 1, size);
+}
+
+void find_distances(float *dis, float current_pos, int *bound) {
+  dis[0] = current_pos - bound[0];
+  dis[1] = abs(bound[1] - current_pos);
+}
+
 image bilinear_resize(image im, int w, int h) {
   image result = make_image(w, h, im.c);
   int channel, row, col;
   int rows[2], cols[2];
   float d_rows[2], d_cols[2];
-  // int lower_r, lower_c, upper_r, upper_c;
   float current_row, current_col;
-  // float d_lower_r, d_upper_r, d_lower_c, d_upper_c;
   float color_value;
   for (channel = 0; channel < im.c; channel++) {
     for (row = 0; row < h; row++) {
@@ -44,34 +52,11 @@ image bilinear_resize(image im, int w, int h) {
         current_row = scale_to_original(row, im.h, h);
         current_col = scale_to_original(col, im.w, w);
 
-        // lower_r = (int)current_row;
-        // lower_c = (int)current_col;
+        find_bound(rows, current_row, h);
+        find_bound(cols, current_col, w);
 
-        // upper_r = cap_index(lower_r + 1, h);
-        // upper_c = cap_index(lower_c + 1, w);
-
-        // d_lower_r = current_row - lower_r;
-        // d_lower_c = current_col - lower_c;
-        // d_upper_r = abs(upper_r - current_row);
-        // d_upper_c = abs(upper_c - current_col);
-
-        // color_value =
-        //     get_pixel(im, lower_c, lower_r, channel) * d_upper_r * d_upper_c
-        //     + get_pixel(im, lower_c, upper_r, channel) * d_lower_r *
-        //     d_upper_c + get_pixel(im, upper_c, lower_r, channel) * d_upper_r
-        //     * d_lower_c + get_pixel(im, upper_c, upper_r, channel) *
-        //     d_lower_r * d_lower_c;
-
-        rows[0] = (int)current_row;
-        cols[0] = (int)current_col;
-
-        rows[1] = cap_index(rows[0] + 1, h);
-        cols[1] = cap_index(cols[0] + 1, w);
-
-        d_rows[0] = current_row - rows[0];
-        d_cols[0] = current_col - cols[0];
-        d_rows[1] = abs(rows[1] - current_row);
-        d_cols[1] = abs(cols[1] - current_col);
+        find_distances(d_rows, current_row, rows);
+        find_distances(d_cols, current_col, cols);
 
         color_value = 0;
         for (r = 0; r < 2; r++) {
