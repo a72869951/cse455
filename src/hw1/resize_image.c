7772b5adc4ee1600078fb84b8bf1a27a71f9f34e
Chanwut Kittivorawong <chanwutk@gmail.com>
2019-10-11 22:00:30 -0700

    resize

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int upper_cap_index(int index, int size) {
-  return index > size - 1 ? index : size - 1;
+  return index < size - 1 ? index : size - 1;
 }
 
 int lower_cap_index(int index) { return index >= 0 ? index : 0; }
@@ -11,24 +11,24 @@ float to_original_scale(int new_index, int original_size, int new_size) {
   return ((new_index + 0.5) * (original_size + 1.0) / (new_size + 1.0)) - 0.5;
 }
 
-float scale_to_new(int original_index, int original_size, int new_size) {
-  return ((original_index + 0.5) * (new_size + 1.0) / (original_size + 1.0)) -
-         0.5;
-}
-
 float nn_interpolate(image im, float x, float y, int c) {
-  return get_pixel(im, (int)round(x), (int)round(y), c);
+  int row, col;
+  col = upper_cap_index(lower_cap_index((int)round(x)), im.w);
+  row = upper_cap_index(lower_cap_index((int)round(y)), im.h);
+  return get_pixel(im, col, row, c);
 }
 
 image nn_resize(image im, int w, int h) {
   image result = make_image(w, h, im.c);
   int channel, row, col;
+  float color_value, row_scaled, col_scaled;
   for (channel = 0; channel < im.c; channel++) {
-    for ( col = 0; col < w; col++) {
+    for (col = 0; col < w; col++) {
       for (row = 0; row < h; row++) {
-         set_pixel(result, col, row, channel,
-                   nn_interpolate(im, to_original_scale(col, im.w, w),
-                                  to_original_scale(row, im.h, h), channel));
+        col_scaled = to_original_scale(col, im.w, w);
+        row_scaled = to_original_scale(row, im.h, h);
+        color_value = nn_interpolate(im, col_scaled, row_scaled, channel);
+        set_pixel(result, col, row, channel, color_value);
       }
     }
   }
@@ -57,8 +57,8 @@ float bilinear_interpolate(image im, float x, float y, int c) {
 
   for (row = 0; row < 2; row++) {
     for (col = 0; col < 2; col++) {
-      color_value += get_pixel(im, cols[col], rows[row], c) * d_rows[2 - row] *
-                     d_cols[2 - col];
+      color_value += get_pixel(im, cols[col], rows[row], c) * d_rows[1 - row] *
+                     d_cols[1 - col];
     }
   }
   return color_value;
@@ -67,13 +67,14 @@ float bilinear_interpolate(image im, float x, float y, int c) {
 image bilinear_resize(image im, int w, int h) {
   image result = make_image(w, h, im.c);
   int channel, row, col;
+  float color_value, row_scaled, col_scaled;
   for (channel = 0; channel < im.c; channel++) {
-    for (row = 0; row < h; row++) {
-      for (col = 0; col < w; col++) {
-        set_pixel(result, col, row, channel,
-                  bilinear_interpolate(im, to_original_scale(col, im.w, w),
-                                       to_original_scale(row, im.h, h),
-                                       channel));
+    for (col = 0; col < w; col++) {
+      for (row = 0; row < h; row++) {
+        col_scaled = to_original_scale(col, im.w, w);
+        row_scaled = to_original_scale(row, im.h, h);
+        color_value = bilinear_interpolate(im, col_scaled, row_scaled, channel);
+        set_pixel(result, col, row, channel, color_value);
       }
     }
   }
