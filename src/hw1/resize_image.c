df2ddfb10f8d57f51e602da7e342ecb5d3305e4e
Chanwut Kittivorawong <chanwutk@gmail.com>
2019-10-11 20:47:54 -0700

    resize

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -1,11 +1,13 @@
 #include "image.h"
 #include <math.h>
 
-int cap_index(int index, int size) {
+int upper_cap_index(int index, int size) {
   return index > size - 1 ? index : size - 1;
 }
 
-float scale_to_original(int new_index, int original_size, int new_size) {
+int lower_cap_index(int index) { return index >= 0 ? index : 0; }
+
+float to_original_scale(int new_index, int original_size, int new_size) {
   return ((new_index + 0.5) * (original_size + 1.0) / (new_size + 1.0)) - 0.5;
 }
 
@@ -15,59 +17,65 @@ float scale_to_new(int original_index, int original_size, int new_size) {
 }
 
 float nn_interpolate(image im, float x, float y, int c) {
-  // TODO Fill in
-  return 0;
+  return get_pixel(im, (int)round(x), (int)round(y), c);
 }
 
 image nn_resize(image im, int w, int h) {
-  // TODO Fill in (also fix that first line)
-  return make_image(1, 1, 1);
-}
-
-float bilinear_interpolate(image im, float x, float y, int c) {
-  // TODO
-  return 0;
+  image result = make_image(w, h, im.c);
+  int channel, row, col;
+  for (channel = 0; channel < im.c; channel++) {
+    for ( col = 0; col < w; col++) {
+      for (row = 0; row < h; row++) {
+         set_pixel(result, col, row, channel,
+                   nn_interpolate(im, to_original_scale(col, im.w, w),
+                                  to_original_scale(row, im.h, h), channel));
+      }
+    }
+  }
+  return result;
 }
 
 void find_bound(int *bound, float current_pos, int size) {
-  bound[0] = (int)current_pos;
-  bound[1] = cap_index(bound[0] + 1, size);
+  bound[0] = lower_cap_index((int)current_pos);
+  bound[1] = upper_cap_index(bound[0] + 1, size);
 }
 
 void find_distances(float *dis, float current_pos, int *bound) {
   dis[0] = current_pos - bound[0];
-  dis[1] = abs(bound[1] - current_pos);
+  dis[1] = fabsf(bound[1] - current_pos);
+}
+
+float bilinear_interpolate(image im, float x, float y, int c) {
+  int rows[2], cols[2], row, col;
+  float d_rows[2], d_cols[2], color_value = 0;
+
+  find_bound(rows, y, im.h);
+  find_bound(cols, x, im.w);
+
+  find_distances(d_rows, y, rows);
+  find_distances(d_cols, x, cols);
+
+  for (row = 0; row < 2; row++) {
+    for (col = 0; col < 2; col++) {
+      color_value += get_pixel(im, cols[col], rows[row], c) * d_rows[2 - row] *
+                     d_cols[2 - col];
+    }
+  }
+  return color_value;
 }
 
 image bilinear_resize(image im, int w, int h) {
   image result = make_image(w, h, im.c);
   int channel, row, col;
-  int rows[2], cols[2];
-  float d_rows[2], d_cols[2];
-  float current_row, current_col;
-  float color_value;
   for (channel = 0; channel < im.c; channel++) {
     for (row = 0; row < h; row++) {
       for (col = 0; col < w; col++) {
-        current_row = scale_to_original(row, im.h, h);
-        current_col = scale_to_original(col, im.w, w);
-
-        find_bound(rows, current_row, h);
-        find_bound(cols, current_col, w);
-
-        find_distances(d_rows, current_row, rows);
-        find_distances(d_cols, current_col, cols);
-
-        color_value = 0;
-        for (r = 0; r < 2; r++) {
-          for (c = 0; c < 2; c++) {
-            color_value += get_pixel(im, cols[c], rows[r], channel) *
-                           d_rows[2 - r] * d_cols[2 - c];
-          }
-        }
-
-        set_pixel(im, col, row, channel, color_value);
+        set_pixel(result, col, row, channel,
+                  bilinear_interpolate(im, to_original_scale(col, im.w, w),
+                                       to_original_scale(row, im.h, h),
+                                       channel));
       }
     }
   }
+  return result;
 }
