23180611e3a57e8381274c62a8b130146318ab96
Chanwut Kittivorawong <chanwutk@gmail.com>
2019-10-21 15:07:01 -0700

    fix resize

--- src/hw1/resize_image.c
+++ src/hw1/resize_image.c
@@ -1,20 +1,18 @@
-#include "image.h"
 #include <math.h>
+#include "image.h"
 
-int upper_cap_index(int index, int size) {
-  return index < size - 1 ? index : size - 1;
+int cap_index(int index, int size) {
+  return index < size - 1 ? index >= 0 ? index : 0 : size - 1;
 }
 
-int lower_cap_index(int index) { return index >= 0 ? index : 0; }
-
 float to_original_scale(int new_index, int original_size, int new_size) {
-  return ((new_index + 0.5) * (original_size + 1.0) / (new_size + 1.0)) - 0.5;
+  return ((new_index + 0.5) * (original_size) / (new_size)) - 0.5;
 }
 
 float nn_interpolate(image im, float x, float y, int c) {
   int row, col;
-  col = upper_cap_index(lower_cap_index((int)round(x)), im.w);
-  row = upper_cap_index(lower_cap_index((int)round(y)), im.h);
+  col = cap_index((int)round(x), im.w);
+  row = cap_index((int)round(y), im.h);
   return get_pixel(im, col, row, c);
 }
 
@@ -23,8 +21,8 @@ image nn_resize(image im, int w, int h) {
   int channel, row, col;
   float color_value, row_scaled, col_scaled;
   for (channel = 0; channel < im.c; channel++) {
-    for (col = 0; col < w; col++) {
-      for (row = 0; row < h; row++) {
+    for (row = 0; row < h; row++) {
+      for (col = 0; col < w; col++) {
         col_scaled = to_original_scale(col, im.w, w);
         row_scaled = to_original_scale(row, im.h, h);
         color_value = nn_interpolate(im, col_scaled, row_scaled, channel);
@@ -36,13 +34,18 @@ image nn_resize(image im, int w, int h) {
 }
 
 void find_bound(int *bound, float current_pos, int size) {
-  bound[0] = lower_cap_index((int)current_pos);
-  bound[1] = upper_cap_index(bound[0] + 1, size);
+  bound[0] = cap_index((int)current_pos, size);
+  bound[1] = cap_index((int)(current_pos + 1), size);
 }
 
 void find_distances(float *dis, float current_pos, int *bound) {
-  dis[0] = current_pos - bound[0];
-  dis[1] = fabsf(bound[1] - current_pos);
+  if (bound[0] == bound[1]) {
+    dis[0] = 0.5;
+    dis[1] = 0.5;
+  } else {
+    dis[0] = current_pos - bound[0];
+    dis[1] = bound[1] - current_pos;
+  }
 }
 
 float bilinear_interpolate(image im, float x, float y, int c) {
@@ -69,8 +72,8 @@ image bilinear_resize(image im, int w, int h) {
   int channel, row, col;
   float color_value, row_scaled, col_scaled;
   for (channel = 0; channel < im.c; channel++) {
-    for (col = 0; col < w; col++) {
-      for (row = 0; row < h; row++) {
+    for (row = 0; row < h; row++) {
+      for (col = 0; col < w; col++) {
         col_scaled = to_original_scale(col, im.w, w);
         row_scaled = to_original_scale(row, im.h, h);
         color_value = bilinear_interpolate(im, col_scaled, row_scaled, channel);
